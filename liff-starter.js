8a732771ad34374253c6609c314d3a352aae848c
phitthaya <37451527+phitthaya@users.noreply.github.com>
2019-10-30 01:54:24 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -27,9 +27,124 @@ function initializeApp(data) {
     // sendMessages call
     document.getElementById('sendmessagebutton').addEventListener('click', function () {
         liff.sendMessages([{
-            type: 'text',
-            text: "You've successfully sent a message! Hooray!"
-        }, {
+  "type": "flex",
+  "altText": "Flex Message",
+  "contents": {
+    "type": "bubble",
+    "hero": {
+      "type": "image",
+      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
+      "size": "full",
+      "aspectRatio": "20:13",
+      "aspectMode": "cover",
+      "action": {
+        "type": "uri",
+        "label": "Action",
+        "uri": "https://linecorp.com"
+      }
+    },
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "spacing": "md",
+      "action": {
+        "type": "uri",
+        "label": "Action",
+        "uri": "https://linecorp.com"
+      },
+      "contents": [
+        {
+          "type": "text",
+          "text": "Brown's Burger",
+          "size": "xl",
+          "weight": "bold"
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "baseline",
+              "contents": [
+                {
+                  "type": "icon",
+                  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_regular_32.png"
+                },
+                {
+                  "type": "text",
+                  "text": "$10.5",
+                  "flex": 0,
+                  "margin": "sm",
+                  "weight": "bold"
+                },
+                {
+                  "type": "text",
+                  "text": "400kcl",
+                  "size": "sm",
+                  "align": "end",
+                  "color": "#AAAAAA"
+                }
+              ]
+            },
+            {
+              "type": "box",
+              "layout": "baseline",
+              "contents": [
+                {
+                  "type": "icon",
+                  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_large_32.png"
+                },
+                {
+                  "type": "text",
+                  "text": "$15.5",
+                  "flex": 0,
+                  "margin": "sm",
+                  "weight": "bold"
+                },
+                {
+                  "type": "text",
+                  "text": "550kcl",
+                  "size": "sm",
+                  "align": "end",
+                  "color": "#AAAAAA"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "text",
+          "text": "Sauce, Onions, Pickles, Lettuce & Cheese",
+          "size": "xxs",
+          "color": "#AAAAAA",
+          "wrap": true
+        }
+      ]
+    },
+    "footer": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "spacer",
+          "size": "xxl"
+        },
+        {
+          "type": "button",
+          "action": {
+            "type": "uri",
+            "label": "Add to Cart",
+            "uri": "https://linecorp.com"
+          },
+          "color": "#905C44",
+          "style": "primary"
+        }
+      ]
+    }
+  }
+}, {
             type: 'sticker',
             packageId: '2',
             stickerId: '144'
